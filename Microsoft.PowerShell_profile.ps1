fec056388bba7a612ac959ccf75f73397c11738a
Deivids Egle <107066953+azdeivids@users.noreply.github.com>
2022-12-24 17:06:31 +0000

    Delete Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++
@@ -1,8 +0,0 @@
-#### wt.exe profile dir ~\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
-
-. $env:USERPROFILE\.config\powershell\user_profile.ps1
-$ENV:STARSHIP_CONFIG = "$HOME\.config\starship\starship.toml"
-$ENV:STARSHIP_DISTRO = " deivids "
-Invoke-Expression (&starship init powershell)
-
-#  者 🪟
