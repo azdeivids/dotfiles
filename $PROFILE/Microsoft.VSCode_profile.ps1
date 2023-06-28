9d83bb0a3955e1824b155dc7143c93e78755ed8e
Deivids Egle <107066953+azdeivids@users.noreply.github.com>
2023-06-28 17:47:55 +0100

    Delete $PROFILE directory

--- $PROFILE/Microsoft.VSCode_profile.ps1
+++
@@ -1,8 +0,0 @@
-### vscode pwsh profile dir ~\Documents\PowerShell\Microsoft.VSCode_profile.ps1
-
-. $env:USERPROFILE\.config\powershell\user_profile.ps1
-$ENV:STARSHIP_CONFIG = "$HOME\.config\starship\starship.toml"
-$ENV:STARSHIP_DISTRO = "者deivids "
-Invoke-Expression (&starship init powershell)
-Import-Module Terminal-Icons
-Import-Module posh-git
