6a129f336b5bf1f93d6eb0e0fced4dd561b2893b
azdeivids <deivids.egle@gmail.com>
2023-10-06 08:46:20 +0100

    Added starship startup

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -2,6 +2,10 @@
 
 [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
 
+# $ENV:STARSHIP_CONFIG = "$HOME\.config\starship\starship.toml"
+# $ENV:STARSHIP_DISTRO = "deivids"
+# Invoke-Expression (&starship init powershell)
+
 oh-my-posh init pwsh | Invoke-Expression
 oh-my-posh init pwsh --config 'C:/Users/deivids/.config/oh-my-posh/mad-theme.omp.json' | Invoke-Expression
 
@@ -31,3 +35,8 @@ Set-Alias time Get-Date
 Set-Alias tf terraform
 Set-Alias d docker
 Set-Alias np notepad
+
+function which ($command) {
+    Get-Command -Name $command -ErrorAction SilentlyContinue |
+      Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+  }
